7f5ba55b17c9c9984069f72201121f398fbe335f
Jeff Scott <jpscott@ranken.edu>
2020-04-01 13:15:53 -0500

    Second commit.  Model package and Model classes created.

---
+++ app/src/main/java/com/example/jeffscottresume/model/ModelProject.java
@@ -0,0 +1,49 @@
+package com.example.jeffscottresume.model;
+
+public class ModelProject {
+    public String projectName;
+    public String projectType;
+    public String projectDescription;
+    public String projectImagePath;
+    public String projectLandingPage;
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectType() {
+        return projectType;
+    }
+
+    public void setProjectType(String projectType) {
+        this.projectType = projectType;
+    }
+
+    public String getProjectDescription() {
+        return projectDescription;
+    }
+
+    public void setProjectDescription(String projectDescription) {
+        this.projectDescription = projectDescription;
+    }
+
+    public String getProjectImagePath() {
+        return projectImagePath;
+    }
+
+    public void setProjectImagePath(String projectImagePath) {
+        this.projectImagePath = projectImagePath;
+    }
+
+    public String getProjectLandingPage() {
+        return projectLandingPage;
+    }
+
+    public void setProjectLandingPage(String projectLandingPage) {
+        this.projectLandingPage = projectLandingPage;
+    }
+}
